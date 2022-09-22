1e1d27b0cd177427fbe392039a0188162535aa7a
Pedro Raimundo <86616758+pedrorneto1@users.noreply.github.com>
2022-09-22 19:30:40 -0300

    hello

---
+++ hello/src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Laboratório de Programação 2 - Lab1
+ * 
+ * @author Pedro Raimundo dos Santos Neto - 120110121
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello Dirlididi!");
+	}
+}
